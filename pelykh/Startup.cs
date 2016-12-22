4fb8d41291d375c513f6b19a324f2fb86efe310c
JohnMosh <375333007284@yandex.by>
2016-12-22 18:28:13 +0300

    first commit

---
+++ pelykh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pelykh.Startup))]
+namespace pelykh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
